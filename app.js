a68154751deb2b96141f693f588afe59226c2ab1
Ben Hagan <benhagan@gmail.com>
2014-03-05 11:11:20 +0000

    Detect last response now within the processor.

--- app.js
+++ app.js
@@ -19,19 +19,15 @@ if (utils.validCache() === false) {
 
 twit.getFollowersIds(config.twitter_id_or_handle, function (err, data) {
 
-    if (err && err.statusCode === 429 && err.data){ // rate limit hit
+    if (err && err.statusCode === 429 && err.data) { // rate limit hit
         console.log('  -- ' + err.data);
     } else if (err) {
         throw err;
     }
 
     if (data) {
-        if (data && data.next_cursor_str && data.next_cursor_str === '0') {
-            console.log('DEBUG: Collection Complete.');
-        } else {
-            processor.process(config.twitter_id_or_handle, data), function (err, data){
-                console.log(data);
-            }
+        processor.process(config.twitter_id_or_handle, data), function (err, data){
+            console.log(data);
         }
     }
 
